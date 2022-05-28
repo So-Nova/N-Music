36118db20fe035a8cc1328f6791995bc5281b937
TEAM NOVA <94800553+Tm-Nova@users.noreply.github.com>
2022-05-28 19:56:03 +0200

    Update admins.py

--- program/admins.py
+++ program/admins.py
@@ -56,7 +56,7 @@ async def update_admin(client, message: Message):
         new_admins.append(u.user.id)
     admins[message.chat.id] = new_admins
     await message.reply_text(
-        "⌯ تم اعادة **تشغيل البوت** \n⌯ وتم **تحديث** قائمة **المشرفين**"
+        "⌯ تم اعادة **تشغيل البوت** \n⌯ تم **تحديث** قائمة **المشرفين**"
     )
 
 
