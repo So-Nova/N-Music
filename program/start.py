21ff53659a3ab9cf4143a28f0ad24704c0297513
TEAM NOVA <94800553+Tm-Nova@users.noreply.github.com>
2022-07-03 02:16:16 +0200

    Update start.py

--- program/start.py
+++ program/start.py
@@ -106,7 +106,7 @@ async def start_(c: Client, message: Message):
 
 
 @Client.on_message(
-    command(["وامر", "لاوامر", f"alive@{BOT_USERNAME}"]) & filters.group & ~filters.edited
+    command(["وامر الاغاني","وامر اغاني", f"alive@{BOT_USERNAME}"]) & filters.group & ~filters.edited
 )
 @check_blacklist()
 async def alive(c: Client, message: Message):
