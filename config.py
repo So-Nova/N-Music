ad637a8c6cfa6302f8dde1d8e54046698c56d9d9
Team Nova <94800553+So-Nova@users.noreply.github.com>
2022-07-21 16:35:35 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,16 +5,16 @@ admins = {}
 load_dotenv()
 
 # client vars
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-BOT_TOKEN = getenv("BOT_TOKEN")
-SESSION_NAME = getenv("SESSION_NAME", "session")
+API_ID = int(getenv("API_ID","11174745"))
+API_HASH = getenv("API_HASH","364a3329a7dc1fa55f8d59229e62b65e")
+BOT_TOKEN = getenv("BOT_TOKEN","5180441164:AAFo3gGSjouNFECJ_Ie931zydnTGjImiMBI")
+SESSION_NAME = getenv("SESSION_NAME", "BABURdPT6QoKvarUId_SBMBh27y_CZpx8qPI6BjV_1GVxtENpaBRqoAYGyAAqymJReBG0ZJMQq4GvewRcLqEVm1BS14hCoJoJvl2gYvEsJgLr-76zm90AnwB_x357LNRRWBdOveu_2Gf0__2jvQW9ioPNnMg9E2PTsIucVkyzhhcZAJKn1c5ADs6qpHEW9Ce4QTFWFSttBRRl7Be-3EyXjK9AImcS7Fj4ethbYgd-AkwRX5RHu4MGZduIC6bWemW2uW3r026si9zn15x4lNmxSTc0ptKA-DtYVNJ3XVRa_Sauy-WzY6Ok39kWVs71ZcmcL-PALBHOtryvXOnn1_RgBbwAAAAAUl3DIQA")
 
 # mandatory vars
-OWNER_USERNAME = getenv("OWNER_USERNAME")
-ALIVE_NAME = getenv("ALIVE_NAME")
-BOT_USERNAME = getenv("BOT_USERNAME")
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/levina-lab/video-stream")
+OWNER_USERNAME = getenv("OWNER_USERNAME","G_W_P")
+ALIVE_NAME = getenv("ALIVE_NAME","Song_Nova")
+BOT_USERNAME = getenv("BOT_USERNAME","TsNoBot")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/So-Nova/iMusic")
 UPSTREAM_BRANCH = getenv("UPSTREM_BRANCH", "main")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
 GROUP_SUPPORT = getenv("GROUP_SUPPORT", "GhNova")
@@ -23,8 +23,8 @@ UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "TmNova")
 # database, decorators, handlers mandatory vars
 MONGODB_URL = getenv("MONGODB_URL")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "ت ا ش و ك ر غ ب ف م / ! . $").split())
-OWNER_ID = list(map(int, getenv("OWNER_ID").split()))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID","5125194988").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS","5527506052").split()))
 
 # image resources vars
 IMG_1 = getenv("IMG_1", "https://telegra.ph/file/c6cc20e377eb6c0f33b07.jpg")
